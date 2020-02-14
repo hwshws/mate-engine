40c8b5688818cc6e85cd9bf5ce050a29a9621752
Heinz-Wilhelm Schäbe <heinz-wilhelm@schaebe.com>
2020-02-14 22:05:20 +0100

    Grundgerüst. Bootstrap, index.php

---
+++ index.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="mate-engine - Getränkeverkauf bei JHULM">
+    <meta name="author" content="hwshws">
+    <link rel="icon" href="https://jugendhackt.org/wp-content/themes/jh-lauch-theme/images/favicon/favicon.ico">
+
+    <title>mate-engine - Getränkeverkauf bei JHULM</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="starter-template.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <a class="navbar-brand" href="#">mate-engine</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="QR scannen" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Kontostand abrufen</button>
+        </form>
+      </div>
+    </nav>
+
+    <main role="main" class="container">
+
+      <div class="starter-template">
+        <h1>Getränkeverkauf <small class="text-muted">Neu, jetzt noch besser!</small></h1>
+        <p class="lead">Teilnehmis und Mentoris haben Durst. Dein Job ist, dem Abhilfe zu schaffen.</p>
+        <a class="btn btn-primary btn-lg btn-block" href="#" role="button">Einzahlung</a>
+        <a class="btn btn-primary btn-lg btn-block" href="#" role="button">Ausgabe</a>
+        <a class="btn btn-primary btn-lg btn-block" href="#" role="button">Abrufen Kontostand (auch oben rechts)</a>
+        <a class="btn btn-danger btn-lg btn-block disabled" href="#" role="button">Leerstandmeldung</a>
+        
+
+      </div>
+
+    </main><!-- /.container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="assets/js/vendor/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
