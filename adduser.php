b534673ed5b8144b116320e2a1e9c8733770bfb4
hws <pepsclena@gmx.de>
2020-08-23 16:47:09 +0200

    more UI Tests

--- adduser.php
+++ adduser.php
@@ -22,22 +22,34 @@ else :
 
         <div class="starter-template">
             <h1>Getränkeverkauf <small class="text-muted">Benutzeranlage</small></h1>
-            <form action="./controller/add.php" method="post">
-                <input type="number" name="amount" placeholder="initialAmount" min="1" /> <br>
-                <div class="input-group input-group-sm mb-3">
-                    <div class="input-group-prepend" >
-                        <span class="input-group-text" id="inputGroup-sizing-sm">initialAmount</span>
-                    </div>
-                    <input type="number" min="1" class="form-control"  name="amount" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
-                </div><br>
-                <input type="text" name="authSecret" placeholder="authSecret" /> <br>
-                <input type="number" name="authCode" placeholder="authCode" /> <br>
-                <input type="text" name="userSecret" placeholder="userSecret" /> <br>
-                <input type="number" name="userCode" placeholder="userCode" min="0000" max="9999" /> <br>
-                <input type="number" name="userCodeDoubleCheck" placeholder="userCodeDoubleCheck" min="0000" max="9999" /> <br>
-                <input type="number" name="permission" placeholder="permission" min="0" max="3"/> <br> <!-- // TODO: Überlegen, welche Premissions für wen... > DOKU -->
-                <input type="submit" value="Nutzer anlegen">
-            </form>
+
+            <div class="row">
+                <div class="col-md-4"></div>
+                <div class="col-md-4">
+
+                    <form action="./controller/add.php" method="post">
+                        <input type="number" name="amount" placeholder="initialAmount" min="1"/> <br>
+                        <div class="input-group input-group-sm mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="inputGroup-sizing-sm">initialAmount</span>
+                            </div>
+                            <input type="number" min="1" class="form-control" name="amount"
+                                   aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">
+                        </div>
+                        <br>
+                        <input type="text" name="authSecret" placeholder="authSecret"/> <br>
+                        <input type="number" name="authCode" placeholder="authCode"/> <br>
+                        <input type="text" name="userSecret" placeholder="userSecret"/> <br>
+                        <input type="number" name="userCode" placeholder="userCode" min="0000" max="9999"/> <br>
+                        <input type="number" name="userCodeDoubleCheck" placeholder="userCodeDoubleCheck" min="0000"
+                               max="9999"/> <br>
+                        <input type="number" name="permission" placeholder="permission" min="0" max="3"/> <br>
+                        <!-- // TODO: Überlegen, welche Premissions für wen... > DOKU -->
+                        <input type="submit" value="Nutzer anlegen">
+                    </form>
+                </div>
+            </div>
+            <div class="col-md-4"></div>
         </div>
 
     </main>
