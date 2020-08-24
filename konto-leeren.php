819bf94b41f83378a8804bb6a771dc37cee1a740
LarsVomMars <lars@kroenner.eu>
2020-08-24 18:44:27 +0200

    Added further redundancy

--- konto-leeren.php
+++ konto-leeren.php
@@ -78,8 +78,6 @@ else :
         form.addEventListener("submit", async e => {
             e.preventDefault();
 
-            const action = form.action;
-
             const authSecret = document.getElementById("authSecret");
             const authCode = document.getElementById("authCode");
             const userSecret = document.getElementById("userSecret");
@@ -93,32 +91,37 @@ else :
                 userCode: userCode.value,
                 balance: balance.value,
             };
-            const resp = await fetch(action, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(data),
-            });
-            const res = await resp.json();
-            if (!res.success) {
-                alert(res.message);
-                return;
-            }
 
-            if (action.endsWith("balance.php")) {
-                balance.parentElement.style.display = "flex";
-                balance.value = res.balance;
+            let cnfirm = true;
+            if (form.action.endsWith("account.php")) cnfirm = confirm(`Guthaben ${balance.value} ausgezahlt?`);
+            if (cnfirm) {
+                const resp = await fetch(form.action, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(data),
+                });
+                const res = await resp.json();
+                if (!res.success) {
+                    alert(res.message);
+                    return;
+                }
 
-                authSecret.disabled = true;
-                authCode.disabled = true;
-                userSecret.disabled = true;
-                userCode.disabled = true;
+                if (form.action.endsWith("balance.php")) {
+                    balance.parentElement.style.display = "flex";
+                    balance.value = res.balance;
 
-                form.action = "controller/emptyaccount.php";
-                document.querySelector("input[type=submit]").value = "Konto leeren";
-            } else {
-                window.location.replace("index.php");
+                    authSecret.disabled = true;
+                    authCode.disabled = true;
+                    userSecret.disabled = true;
+                    userCode.disabled = true;
+
+                    form.action = "controller/emptyaccount.php";
+                    document.querySelector("input[type=submit]").value = "Konto leeren";
+                } else {
+                    window.location.replace("index.php");
+                }
             }
         });
     </script>
