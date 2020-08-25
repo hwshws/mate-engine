4ec86569afb892b7a0c5067a7f872d725ad5a48f
Lars K <lars@kroenner.eu>
2020-08-25 22:29:51 +0200

    AHHHHH )

--- db/dbconnector.php
+++ db/dbconnector.php
@@ -3,7 +3,7 @@ require_once "credentials.php";
 $dsn = PROTOCOL . ":host=" . HOST . ";dbname=" . DBNAME . ";charset=" . CHARSET;
 
 try {
-    $pdo = new PDO($dsn, USERNAME, PASSWORD));
+    $pdo = new PDO($dsn, USERNAME, PASSWORD);
     $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
