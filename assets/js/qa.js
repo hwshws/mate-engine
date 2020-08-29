af5d4cd4f056df05f572a33b614a24b68a0ebc9e
LarsVomMars <lars@kroenner.eu>
2020-08-29 22:43:04 +0200

    Script tags to js files

---
+++ assets/js/qa.js
@@ -0,0 +1,15 @@
+const quickBalanceForm = document.getElementById("quick-balance");
+if (quickBalanceForm) quickBalanceForm.addEventListener('submit', async e => {
+    e.preventDefault();
+    const secret = document.getElementById("qa-qr").value;
+    const resp = await fetch("controller/quickAccess.php", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({secret}),
+    });
+    const res = await resp.json();
+    if (res.success) alert("Guthaben: " + res.balance);
+    else console.log(res.message);
+});
--- konto-leeren.php
+++ konto-leeren.php
@@ -72,60 +72,7 @@ else :
         </div>
     </main>
     <?php include("includes/footer.php"); ?>
-    <script>
-        const form = document.getElementById("balance-form");
-
-        form.addEventListener("submit", async e => {
-            e.preventDefault();
-
-            const authSecret = document.getElementById("authSecret");
-            const authCode = document.getElementById("authCode");
-            const userSecret = document.getElementById("userSecret");
-            const userCode = document.getElementById("userCode");
-            const balance = document.getElementById("balance");
-
-            const data = {
-                authSecret: authSecret.value,
-                authCode: authCode.value,
-                userSecret: userSecret.value,
-                userCode: userCode.value,
-                balance: balance.value,
-            };
-
-            let cnfirm = true;
-            if (form.action.endsWith("account.php")) cnfirm = confirm(`Guthaben ${balance.value} ausgezahlt?`);
-            if (cnfirm) {
-                const resp = await fetch(form.action, {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(data),
-                });
-                const res = await resp.json();
-                if (!res.success) {
-                    // TODO: Error handling
-                    alert(res.message);
-                    return;
-                }
-
-                if (form.action.endsWith("balance.php")) {
-                    balance.parentElement.style.display = "flex";
-                    balance.value = res.balance;
-
-                    authSecret.disabled = true;
-                    authCode.disabled = true;
-                    userSecret.disabled = true;
-                    userCode.disabled = true;
-
-                    form.action = "controller/emptyaccount.php";
-                    document.querySelector("input[type=submit]").value = "Konto leeren";
-                } else {
-                    window.location.replace("index.php");
-                }
-            }
-        });
-    </script>
+    <script src="assets/js/empty.js"></script>
     </body>
     </html>
 <?php
