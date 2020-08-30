1fe035e4802056bf111a515e0e6802bc2c1df89d
LarsVomMars <lars@kroenner.eu>
2020-08-31 01:01:14 +0200

    Quick balance access with sweetalert

--- assets/js/qa.js
+++ assets/js/qa.js
@@ -1,16 +1,41 @@
-// TODO: Button
-const quickBalanceForm = document.getElementById("quick-balance");
-if (quickBalanceForm) quickBalanceForm.addEventListener('submit', async e => {
-    e.preventDefault();
-    const secret = document.getElementById("qa-qr").value;
-    const resp = await fetch("controller/quickAccess.php", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
+const quickAccessButton = document.getElementById("quick-access-button");
+if (quickAccessButton) quickAccessButton.addEventListener('click', async (e) => {
+    const result = await Swal.fire({
+        title: "QR-Code",
+        input: "text",
+        inputAttributes: {autocapitalize: "off"},
+        showCancelButton: true,
+        confirmButtonText: "Kontostand abrufen",
+        showLoaderOnConfirm: true,
+        preConfirm: async (secret) => {
+            const resp = await fetch("controller/quickAccess.php", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: JSON.stringify({secret}),
+            });
+            return await resp.json();
         },
-        body: JSON.stringify({secret}),
+        allowOutsideClick: () => !Swal.isLoading(),
     });
-    const res = await resp.json();
-    if (res.success) alert("Guthaben: " + res.balance);
-    else console.log(res.message);
+    if (result.value) {
+        if (result.value.success) {
+            Swal.fire({
+                icon: "success",
+                title: "Anfrage erfolgreich!",
+                text: `Der Kontostand beträgt: ${result.value.balance}`,
+            });
+        } else {
+            Swal.fire({
+                icon: "error",
+                title: "Ein Fehler ist aufgetreten!",
+                text: result.value.message,
+            });
+        }
+    } else {
+        Swal.fire({
+            icon: "error",
+            title: "Ein Fehler ist aufgetreten!",
+            text: result.value.message,
+        });
+    }
 });
--- controller/quickAccess.php
+++ controller/quickAccess.php
@@ -1,12 +1,22 @@
 <?php
 require_once "../db/dbconnector.php";
 require_once "../db/dbController.php";
+require_once "../lib.php";
 header('Content-Type: application/json');
 
 $post = json_decode(file_get_contents('php://input'), true);
 
-try {
-    echo json_encode(array("success" => true, "balance" => dbController::getUserBalanceBySecret($pdo, $post["secret"])));
-} catch (PDOException $e) {
-    echo json_encode(array("success" => false, "message" => $e));
+if (checkPost($post, "secret")) {
+    try {
+        $balance = dbController::getUserBalanceBySecret($pdo, $post["secret"]);
+        if ($balance) {
+            echo json_encode(array("success" => true, "balance" => $balance));
+        } else {
+            echo json_encode(array("success" => false, "message" => "User not found"));
+        }
+    } catch (PDOException $e) {
+        echo json_encode(array("success" => false, "message" => $e));
+    }
+} else {
+    echo json_encode(array("success" => false, "message" => "Wrong parameter(s)"));
 }
